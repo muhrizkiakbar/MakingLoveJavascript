e517316d302ad150142d6b0e58e35760d885cdd7
muhrizkiakbar <rizkiakbarzein@gmail.com>
2020-08-03 02:48:29 +0800

    variable chapter

---
+++ basic/01-variable/01-console/console.js
@@ -0,0 +1,37 @@
+// Semua web browser modern, node js dan hampir setiap javascript lainnya didukung untuk penulisan pesan logging,
+// berikut ini contoh penulisan log
+//
+
+console.log("Hello world!")
+
+// untuk mencoba fitur logging ini dapat mengakses halaman ini https://jsbin.com/?html,console,output
+
+// jika ingin menampilkan waktu dalam satu baris di console log bisa menggunakan contoh seperti ini
+
+var varItu = "VarItu"
+var varIni = "VarIni"
+
+console.log("varItu:", varItu, "varIni:", varIni)
+
+// maka menghasilkan output
+//  varItu: "VarItu". varIni: "VarIni"
+
+
+// kamu juga bisa menggunakan placeholder pada console.log()
+
+var firstName = "M. Rizki", lastName = "Akbar";
+console.log("%s, %s!",firstName, lastName)
+
+
+// console log object
+// kita dapat membuat console log berupa object, mirip kaya format json bukan? hehehe
+
+console.log({
+  'email':'',
+  'id': 33,
+  'IsHiddenInUI': false,
+  'IsSiteAdmin': false,
+})
+
+
+
